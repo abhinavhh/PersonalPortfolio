f968ca27b1d70c8c5e5ca0da79bcb2a29b8064a2
Adarsh <adarshkannanvfc@gmail.com>
2025-06-28 17:54:44 +0530

    alight adjust at hero secction

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
     }
   return (
     <section id='home' className='mt-16 flex flex-col-reverse justify-center items-center md:flex-row md:gap-12 overflow-x-hidden px-4 md:mt-24'>
-        <div className="flex flex-col items-center gap-4 md:items-start">
+        <div className="flex flex-col gap-4 md:items-start">
             <div className="mt-6 text-justify flex flex-col gap-2 text-base">
                 <h1 className='text-5xl flex gap-3 md:flex-col font-bold'>
                     Hey, <span className='text-5xl font-bold text-blue-800'>
@@ -23,19 +23,22 @@ const Hero = () => {
                 </p>
                 Full-Stack Developer | Problem Solver | Tech Enthusiast
             </div>
-            <div className='flex items-center justify-center gap-4 md:ml-6'>
-                <SocialButton icon={GitHub} label="GitHub" link="https://github.com/abhinavhh" color="#00000"/>
-                <SocialButton icon={LinkedIn} label="GitHub" link="https://www.linkedin.com/in/abhinav-u-212b64286/" color="#00000"/>
-                <SocialButton icon={Instagram} label="GitHub" link="https://www.instagram.com/abhinavanil1830?igsh=MTJoc3hjeWlmbmRtag==" color="#00000"/>
+            <div className='flex gap-8'>
+                <div className='flex items-start gap-4 '>
+                    <SocialButton icon={GitHub} label="GitHub" link="https://github.com/abhinavhh" color="#00000"/>
+                    <SocialButton icon={LinkedIn} label="GitHub" link="https://www.linkedin.com/in/abhinav-u-212b64286/" color="#00000"/>
+                    <SocialButton icon={Instagram} label="GitHub" link="https://www.instagram.com/abhinavanil1830?igsh=MTJoc3hjeWlmbmRtag==" color="#00000"/>  
+                </div>
+                <div>
+                    <button 
+                        className="border-2 px-2 py-0.5 rounded-2xl text-cyan-400 bg-black font-bold md:ml-6"
+                        onClick={handleResumeOpen}
+                    >
+                        Resume
+                    </button>
+                </div>
             </div>
-            <div>
-                <button 
-                    className="border-2 px-2 py-0.5 rounded-2xl text-cyan-400 bg-black font-bold md:ml-6"
-                    onClick={handleResumeOpen}
-                >
-                    Resume
-                </button>
-            </div>
+            
         </div>
         <div className="md:ml-4">
             <img src={heroImage} 
